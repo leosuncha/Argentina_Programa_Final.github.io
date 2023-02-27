a04d91158aeb83dab7bfca915d0bbeea8e70fda2
Leonardo Burgos <leoburgos_13@hotmail.com>
2023-02-27 19:35:33 -0300

    Sexto Commit Proyecto Final(Probando Full Responsive)

--- Js/funciones.js
+++ Js/funciones.js
@@ -7,3 +7,18 @@ document.getElementById('boton').addEventListener('click', function(){
     console.log("Se ha ingresado un número telefónico");
     document.getElementById("demo").innerHTML = "Me comunicaré contigo a la brevedad";
 })*/ 
+
+var x = document.getElementById("demo2");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
