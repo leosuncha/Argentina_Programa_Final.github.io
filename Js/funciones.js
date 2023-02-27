5c961a2d1b9739b1f24f3567ab63a5fc9b079125
Leonardo Burgos <leoburgos_13@hotmail.com>
2023-02-27 01:24:12 -0300

    Quinto Commit Proyecto Final

---
+++ Js/funciones.js
@@ -0,0 +1,9 @@
+document.getElementById('boton').addEventListener('click', function(){
+    console.log("Se ha ingresado un número telefónico");
+    document.getElementById("demo").style.display='contents';
+ })
+
+/**document.getElementById('boton').addEventListener('click', function(){
+    console.log("Se ha ingresado un número telefónico");
+    document.getElementById("demo").innerHTML = "Me comunicaré contigo a la brevedad";
+})*/ 
